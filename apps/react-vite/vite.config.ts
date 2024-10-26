18aaa1594825b6c4ae81576dec64bbe5a69a6f83
Haku <hakudevtw@gmail.com>
2024-10-26 12:48:42 +0800

    feat: adjust devtools config

--- apps/react-vite/vite.config.ts
+++ apps/react-vite/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [tsconfigPaths(), TanStackRouterVite(), react()],
 });
