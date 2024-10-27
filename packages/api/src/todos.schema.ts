e1e1ed0b12604a2c28b533254dc7c81b3de68766
Haku <hakudevtw@gmail.com>
2024-10-27 19:12:32 +0800

    feat: update db todo

--- packages/api/src/todos.schema.ts
+++ packages/api/src/todos.schema.ts
@@ -1,9 +1,15 @@
 import { z } from "zod";
 
+export const Status = z.union([z.literal("active"), z.literal("completed")]);
+export type Status = z.infer<typeof Status>;
+
 export const Todo = z.object({
   id: z.string(),
-  text: z.string(),
+  title: z.string(),
+  content: z.string(),
   completed: z.boolean(),
+  status: Status,
+  due_date: z.date(),
+  userId: z.string(),
 });
-
 export type Todo = z.infer<typeof Todo>;
