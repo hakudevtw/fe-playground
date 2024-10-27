fc5ab86b91f991a9179596e1afac0b235035bffe
Haku <hakudevtw@gmail.com>
2024-10-27 18:53:34 +0800

    chore: update folders

--- apps/vue/src/main.ts
+++ apps/vue/src/main.ts
@@ -1,11 +1,8 @@
 import { createApp } from "vue";
-import "normalize.css";
-import setupPrimeVue from "@/lib/primevue";
 import router from "@/router";
 import "@/assets/index.css";
 import App from "./App.vue";
 
 const app = createApp(App);
-setupPrimeVue(app);
 app.use(router);
 app.mount("#app");
