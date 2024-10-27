ae1cc4e00ff8bb8019d92e1ff2d644fe029361e8
Haku <hakudevtw@gmail.com>
2024-10-27 19:26:18 +0800

    feat: add components

--- packages/api/src/todos.schema.ts
+++ packages/api/src/todos.schema.ts
@@ -1,15 +1,14 @@
 import { z } from "zod";
 
-export const Status = z.union([z.literal("active"), z.literal("completed")]);
+export const Status = z.enum(["not_started", "in_progress", "completed"]).default("not_started");
 export type Status = z.infer<typeof Status>;
 
 export const Todo = z.object({
   id: z.string(),
   title: z.string(),
-  content: z.string(),
-  completed: z.boolean(),
+  content: z.optional(z.string()),
   status: Status,
-  due_date: z.date(),
+  due_date: z.optional(z.date()),
   userId: z.string(),
 });
 export type Todo = z.infer<typeof Todo>;
